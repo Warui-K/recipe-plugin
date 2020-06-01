a7d72e08ff97d4a8f466818592a84b295178ca3b
Warui-K <warui.kamiri@gmail.com>
2020-06-01 12:31:14 +0300

    #2 Added activation and security features

--- index.php
+++ index.php
@@ -10,3 +10,28 @@
  * Credits URI: https://www.udemy.com/user/luisramirezjr/
  * text domain: recipe
  */
+
+
+ /**
+  * WP loads its own files and constants before loading plugins so that they are available 
+ */
+ if(!function_exists('add_action')){
+     echo "Hi there! I'm just a plugin not much I can do when called directly.";
+     exit;
+
+ }
+
+ /**
+  * Plugin activation
+*/
+ //Setup Section
+
+
+ //Includes Section
+ include('includes/activate.php');
+
+
+ //Hooks Section
+ register_activatiob_hook(__FILE__, 'r_activate_plugin');
+
+ //Shortcodes
