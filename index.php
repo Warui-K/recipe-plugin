a0b38abee4b060e588cd42f91fa3baa98f09c0ea
Warui-K <warui.kamiri@gmail.com>
2020-06-01 04:37:14 +0300

    #1 Develop starter templates

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * Plugin Name: Recipe
+ * Description : A simple WordPress plugin that allowsa user to create recipes and rate those recipes
+ * Version: 1.0
+ * Author: Warui Kamiri
+ * Author URI: https://github.com/Warui-K
+ * Credits:  Luis Ramirez Jr
+ * Credits URI: https://www.udemy.com/user/luisramirezjr/
+ * text domain: recipe
+ */
