6fe6f1f9c46990b0a006f4d81124bd66936b381c
Warui-K <warui.kamiri@gmail.com>
2020-06-04 02:21:54 +0300

    #5 Applying filter hooks to filter through content

---
+++ process/recipe-template.php
@@ -0,0 +1,9 @@
+<ul class="list-unstyled">
+    <li>
+        <strong>RATE_I18N:
+        <!--I18N means internationalization. It is a term for making your code translatable-->
+            <div id='recipe-rating' class="rateit" data-rateit-resetable="false" data-rid="RECIPE_ID">
+            </div>
+        </strong>
+    </li>
+</ul>
